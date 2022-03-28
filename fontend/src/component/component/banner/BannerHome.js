fe413481c6c3672f227307c7f0749629199f2f5a
HuskyPC <binh1432001@gmail.com>
2022-03-28 11:58:56 +0700

    push date 28/03/2022

---
+++ fontend/src/component/component/banner/BannerHome.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Fragment } from "react";
+
+const BannerHome = () => {
+  return (
+    <Fragment>
+      <section className=" banner  bg-red-500">
+        <div className=" w-full relative">
+          <img
+            className=""
+            src="asset/img/banner/test/bckg-2.jpg"
+            alt="hinh ảnh"
+          />
+        </div>
+      </section>
+    </Fragment>
+  );
+};
+
+export default BannerHome;
