b786caa41a8427cf174d261387268616b88de50e
HuskyPC <binh1432001@gmail.com>
2022-03-30 22:36:09 +0700

    date 30/03/2022

--- fontend/src/component/component/banner/BannerHome.js
+++ fontend/src/component/component/banner/BannerHome.js
@@ -9,14 +9,26 @@ import "swiper/css/navigation";
 import "swiper/css";
 import { Autoplay, Pagination, Navigation } from "swiper";
 // import { Link } from "react-router-dom";
+// import useSWR from "swr";
+// import { fetcher } from "../../../config/Config";
+
 const BannerHome = () => {
   const [...dataTestBanner] = BannerFakeData;
-
+  // const [dt, setdt] = useState();
+  // const { data, error } = useSWR(
+  //   "https://localhost:44379/Banner/getBannerAll",
+  //   fetcher
+  // );
+  // console.log("🚀 ~ file: BannerHome.js ~ line 19 ~ BannerHome ~ data", data);
+  // useEffect(()=>{
+  //   setdt)
+  // },[])
   return (
     <Fragment>
-      <section className=" banner  bg-black">
+      {/* <BannerAPI></BannerAPI> */}
+      <section className=" banner  bg-black ">
         <Swiper
-          paceBetween={30}
+          pacebetween={30}
           centeredSlides={true}
           autoplay={{
             delay: 5000,
@@ -28,22 +40,26 @@ const BannerHome = () => {
           navigation={true}
           modules={[Autoplay, Pagination, Navigation]}
         >
-          {dataTestBanner.map((item, index) => (
-            <SwiperSlide>
-              <BannerItem
-                key={item.id}
-                id={item.id}
-                name={item.name}
-                imgDesktop={item.imgDesktop}
-                imgTablet={item.imgTablet}
-                imgMobile={item.imgMobile}
-                url={item.url}
-                contentUrrl={item.contentUrrl}
-                detail1={item.detail1}
-                detail2={item.detail2}
-              ></BannerItem>
-            </SwiperSlide>
-          ))}
+          {dataTestBanner.length > 0 &&
+            dataTestBanner.map((item, index) => (
+              <SwiperSlide key={item.id}>
+                <BannerItem
+                  key={item.id}
+                  id={item.id}
+                  name={item.name}
+                  imgDesktop={item.imgDesktop}
+                  urlImgDesktop={item.urlImgDesktop}
+                  imgTablet={item.imgTablet || item.imgDesktop}
+                  urlImgTablet={item.urlImgTablet || item.urlImgDesktop}
+                  imgMobile={item.imgMobile || item.imgDesktop}
+                  urlImgMobile={item.urlImgMobile || item.urlImgDesktop}
+                  url={item.url}
+                  contentUrrl={item.contentUrrl}
+                  detail1={item.detail1}
+                  detail2={item.detail2}
+                ></BannerItem>
+              </SwiperSlide>
+            ))}
         </Swiper>
       </section>
     </Fragment>
--- fontend/src/index.js
+++ fontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.scss";
+import "../src/component/css/styleAll.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
